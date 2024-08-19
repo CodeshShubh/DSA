7ae1189afd4d27507dcff65639948a1080941b99
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-08-19 12:19:16 +0530

    Selection Short.js

---
+++ Selection_Short.js
@@ -0,0 +1,19 @@
+let array = [2, 35, 1, 6, 4, 98, 10, 20];
+
+function shortArray(data){
+for(let i = 0; i<data.length; i++){
+    let minIndex = i;
+
+    for(let j=i+1; j<data.length; j++){
+        if(data[j]<data[minIndex]){
+              minIndex=j;
+        }
+    }
+    let temp = data[minIndex];
+    data[minIndex]= data[i];
+    data[i]=temp;
+   }
+}
+
+shortArray(array);
+console.log(array)
