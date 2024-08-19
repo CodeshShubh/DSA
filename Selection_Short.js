4eea154601670582db17909855e1f508da314559
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-08-19 12:35:33 +0530

    Selection_Short decreasing

--- Selection_Short.js
+++ Selection_Short.js
@@ -1,19 +1,43 @@
+// let array = [2, 35, 1, 6, 4, 98, 10, 20];
+
+// function shortArray(data){
+// for(let i = 0; i<data.length; i++){
+//     let minIndex = i;
+
+//     for(let j=i+1; j<data.length; j++){
+//         if(data[j]<data[minIndex]){
+//               minIndex=j;
+//         }
+//     }
+//     let temp = data[minIndex];
+//     data[minIndex]= data[i];
+//     data[i]=temp;
+//    }
+// }
+
+// shortArray(array);
+// console.log(array);
+
+
+// Selection_Short Decreseing order
+
 let array = [2, 35, 1, 6, 4, 98, 10, 20];
 
-function shortArray(data){
-for(let i = 0; i<data.length; i++){
+function DecreseingShort(data){
+for(i=0; i<data.length; i++){
     let minIndex = i;
-
-    for(let j=i+1; j<data.length; j++){
-        if(data[j]<data[minIndex]){
-              minIndex=j;
+    for(j=i+1; j<data.length; j++){
+        if(data[j]>data[minIndex]){
+            minIndex=j
         }
     }
     let temp = data[minIndex];
     data[minIndex]= data[i];
-    data[i]=temp;
-   }
+    data[i]= temp;
+}
 }
 
-shortArray(array);
+DecreseingShort(array);
 console.log(array)
+
+
